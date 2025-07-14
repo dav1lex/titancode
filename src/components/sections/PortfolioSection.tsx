6b36f01d303c758a28a373b16ba400dd109394e5
dav1lex <zedxurl@gmail.com>
2025-07-14 11:05:03 +0200

    m1

--- src/components/sections/PortfolioSection.tsx
+++ src/components/sections/PortfolioSection.tsx
@@ -3,7 +3,6 @@
 import { useLanguage } from "@/app/language-context";
 import { motion } from "framer-motion";
 import { ArrowRight } from "lucide-react";
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
