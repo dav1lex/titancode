f049f47ecf4c987880e67e325208b2ac3e5f1eb1
dav1lex <zedxurl@gmail.com>
2025-07-17 11:54:27 +0200

    seo2

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,35 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/about',
+        destination: '/en/about',
+        permanent: true,
+      },
+      {
+        source: '/services',
+        destination: '/en/services',
+        permanent: true,
+      },
+      {
+        source: '/portfolio',
+        destination: '/en/portfolio',
+        permanent: true,
+      },
+      {
+        source: '/contact',
+        destination: '/en/contact',
+        permanent: true,
+      },
+      {
+        source: '/calculate-estimate',
+        destination: '/en/calculate-estimate',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
